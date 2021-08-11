89e13f21762197ecdc9bb237a81196f61f7f4911
Karan Odedra <karanodedra81@gmail.com>
2021-08-11 16:53:19 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
 # modal-project
 
+Basic modal project using below Vue3 concepts:
+-Scoped and global css
+-Props
+-Emitting custom events
+-Click event modifiers
+-Slots (Default and Named)
+-Teleport
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <!-- <Modal heading="Sign up for the Givaway!" text="Grab your ninja swag for half price!" /> -->
+  <teleport to="#modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <!-- Named Slot = links -->
+        <a href="#">Sign Up</a>
+        <a href="#">More Info</a>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag</p>
+    </Modal>
+  </teleport>
+
+  <teleport to="#modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up to the Newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </teleport>
+
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModalTwo">open modal 2</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/Modal';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App!',
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +56,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
